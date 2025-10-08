0b5888045909ce53f38af0ecddadc4a6c2b2568e
Num1s <krutojmer76@gmail.com>
2025-10-08 14:38:42 +0600

    feat(LeetCode): new problem solving

---
+++ main/src/LeetCode/AddTwoNumbers.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+import LeetCode.utils.ListNode;
+import java.util.Arrays;
+
+public class AddTwoNumbers {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        System.out.print(addTwoNumbers(l1, l2).val);
+    }
+}
